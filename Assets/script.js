e179f8f5b7b485391ebdf5753083ed643084f301
heywardhagenbuch <heyward360@gmail.com>
2022-07-19 21:23:26 -0400

    working on forecast

---
+++ Assets/script.js
@@ -0,0 +1,76 @@
+let weather = {
+    apiKey: "f2da475e3f33a76106dd40ea31ab6cfa\n",
+    fetchCity: function (city) {
+        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" +
+            city + "&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No Coordinates Found");
+                    throw new Error("No city found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=imperial&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        const {lon, lat} = data.coord;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".lat").innerText = "Latitude: " + lat;
+        document.querySelector(".lon").innerText = "Longitude: " + lon;
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x900/?nature,city" + name + "')";
+    },
+    displayForecast: function (data) {
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+        this.fetchCity(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("Miami");
