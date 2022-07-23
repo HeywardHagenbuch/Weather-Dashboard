65dd1fc380c818a5f0a2e1353adb3ae71ee95494
heywardhagenbuch <heyward360@gmail.com>
2022-07-23 13:00:26 -0400

    finalized weather widgit

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,6 @@
-let weather = {
-    apiKey: "f2da475e3f33a76106dd40ea31ab6cfa\n",
-    fetchCity: function (city) {
-        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" +
-            city + "&appid=" +
-            this.apiKey
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    alert("No Coordinates Found");
-                    throw new Error("No city found.");
-                }
-                return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-    },
 
+let weather = {
+    apiKey: "f2da475e3f33a76106dd40ea31ab6cfa", //API goes here
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -32,11 +18,10 @@ let weather = {
             .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        const {lon, lat} = data.coord;
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src =
             "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -46,18 +31,10 @@ let weather = {
             "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText =
             "Wind speed: " + speed + " km/h";
-        document.querySelector(".lat").innerText = "Latitude: " + lat;
-        document.querySelector(".lon").innerText = "Longitude: " + lon;
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage =
-            "url('https://source.unsplash.com/1600x900/?nature,city" + name + "')";
-    },
-    displayForecast: function (data) {
-
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
-        this.fetchCity(document.querySelector(".search-bar").value);
     },
 };
 
@@ -74,3 +51,5 @@ document
     });
 
 weather.fetchWeather("Miami");
+
+//API + city + whatever other parameters needed for other API
